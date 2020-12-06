dbbcb8a40bd899c510cd199e1660b964667ce535
Michael Harty <michaelharty3@gmail.com>
2020-12-05 22:41:22 -0700

    day 6

---
+++ 2020/day-6.py
@@ -0,0 +1,41 @@
+import numpy as np
+from collections import Counter
+
+def solve1(input_data):
+    groups = input_data.split('\n\n')
+    return np.sum([len(set(group).difference(set('\n'))) for group in groups])
+
+def solve2(input_data):
+    groups = input_data.split('\n\n')
+    group_size = [len(g.split('\n')) for g in groups] # how many members in each group
+    counters = [Counter(g.replace('\n', '')) for g in groups] # count of each non new line char in each group
+    
+    # for each group, find the number of times a char count equals the member count
+    return np.sum([list(c.values()).count(gs) for gs, c in zip(group_size, counters)])
+
+test_data = """abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b"""
+
+if __name__ == "__main__":
+    from aocd.models import Puzzle
+
+    assert solve1(test_data) == 11
+    assert solve2(test_data) == 6
+    
+    puz6 = Puzzle(2020, 6)
+    data = puz6.input_data
+    puz6.answer_a = solve1(data)
+    puz6.answer_b = solve2(data)
