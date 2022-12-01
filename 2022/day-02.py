4591b2992e83d40ac81b6f1a347b5db7d6a9fcd3
Michael Harty <michaelharty3@gmail.com>
2022-12-01 12:12:30 -0700

    day 2 part 2

--- 2022/day-02.py
+++ 2022/day-02.py
@@ -10,6 +10,40 @@ def parse(input_data):
     return [game.split() for game in translated.splitlines()]
 
 
+def parse2(input_data):
+    translated = (input_data.replace('A', 'rock')
+                        .replace('B', 'paper')
+                        .replace('C', 'scissors')
+                        .replace('X', 'lose')
+                        .replace('Y', 'draw')
+                        .replace('Z', 'win')
+                    )
+    return [game.split() for game in translated.splitlines()]
+
+
+def required_play(p1, outcome):
+    if outcome == 'draw':
+        return p1
+
+    elif outcome == 'win':
+        if p1 == 'rock':
+            return 'paper'
+        if p1 == 'paper':
+            return 'scissors'
+        if p1 == 'scissors':
+            return 'rock'
+    
+    elif outcome == 'lose':
+        if p1 == 'rock':
+            return 'scissors'
+        if p1 == 'paper':
+            return 'rock'
+        if p1 == 'scissors':
+            return 'paper'
+    else:
+        raise ValueError
+    
+
 def game(p1, p2):
     """ return the points awarded for the second player"""
 
@@ -48,6 +82,13 @@ def solve1(input_data):
     return sum([game(*plays) for plays in parsed])
 
 
+def solve2(input_data):
+    parsed = parse2(input_data)
+    plays = [[p1, required_play(p1, result)] for p1, result in parsed]
+    return sum([game(*plays) for plays in plays])
+
+
+
 if __name__ == '__main__':
 
     test_data = """A Y
@@ -56,7 +97,7 @@ C Z
 """
     
     assert solve1(test_data) == 15
-    # assert solve2(test_data) == 45000
+    assert solve2(test_data) == 12
 
     from aocd.models import Puzzle
     puzzle = Puzzle(2022, 2)
@@ -65,8 +106,8 @@ C Z
     print(answer_1)
     puzzle.answer_a = answer_1
 
-    # answer_2 = solve2(puzzle.input_data)
-    # puzzle.answer_b = answer_2
+    answer_2 = solve2(puzzle.input_data)
+    puzzle.answer_b = answer_2
 
 
 
