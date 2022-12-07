1670236d73499ba2d4742c5d893d882e82cf178a
Michael Harty <michaelharty3@gmail.com>
2022-12-06 21:26:00 -0700

    do again with a deque

--- 2022/day-06.py
+++ 2022/day-06.py
@@ -1,3 +1,4 @@
+from collections import deque
 
 def first_data_loc(stream):
     for i, (bits) in enumerate( zip(stream,
@@ -17,10 +18,24 @@ def first_message_loc(stream, n=14):
             return i + n
 
 
+def first_message_loc_deque(stream, n):
+    test_bits = deque(stream[:n], maxlen=n)
+    remaining_bits = deque(stream[n:])
+
+    i = n
+    while len(set(test_bits)) != n:
+        test_bits.append(remaining_bits.popleft())
+        i += 1
+    return i
+
+
 if __name__ == '__main__':
 
     assert first_data_loc('bvwbjplbgvbhsrlpgdmjqwftvncz') == 5
     assert first_message_loc('mjqjpqmgbljsphdztnvjfqwrcgsmlb') == 19
+
+    assert first_message_loc_deque('bvwbjplbgvbhsrlpgdmjqwftvncz', n=4) == 5
+    assert first_message_loc('mjqjpqmgbljsphdztnvjfqwrcgsmlb', n=14) == 19
 
     from aocd.models import Puzzle
     puzzle = Puzzle(2022, 6)
